d87962caa161eb9d243d5a4868443b3847ae288a
파차 <fcreloadeD@outlook.kr>
2015-09-25 16:02:13 +0900

    Reset the

--- src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/DB/AbilityData.java
+++
@@ -1,8 +0,0 @@
-package com.naver.cafe.craftproducer.heptagram.theomachy.db;
-
-
-public interface AbilityData {
-    int ABILITY_NUMBER = 31;
-    int GOD_ABILITY_NUMBER = 12;
-    int HUMAN_ABILITY_NUMBER = 19;
-}
--- src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/DB/PluginData.java
+++
@@ -1,7 +0,0 @@
-package com.naver.cafe.craftproducer.heptagram.theomachy.db;
-
-
-public interface PluginData {
-    final static String version = "1.4-SNAPSHOT"; 
-    final static String buildnumber = "20150917";
-}
