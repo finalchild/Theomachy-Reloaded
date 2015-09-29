235a37b7da7089d611e654cbeefa2875449e296f
Final Child <fcreloaded@outlook.kr>
2015-09-29 19:30:06 +0900

    Version bump

--- src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/TheomachyArena.java
+++ src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/TheomachyArena.java
@@ -3,11 +3,12 @@ package com.naver.cafe.craftproducer.heptagram.theomachy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comze_instancelabs.minigamesapi.Arena;
+import com.comze_instancelabs.minigamesapi.ArenaType;
 
 public class TheomachyArena extends Arena {
 
 	public TheomachyArena(JavaPlugin plugin, String name) {
-		super(plugin, name);
+		super(plugin, name, ArenaType.REGENERATION);
 	}
 	
 }
--- src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/db/PluginData.java
+++ src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/db/PluginData.java
@@ -1,6 +1,6 @@
 package com.naver.cafe.craftproducer.heptagram.theomachy.db;
 
 public interface PluginData {
-    public static final String VERSION = "1.4"; 
+    public static final String VERSION = "2.0-SNAPSHOT"; 
     public static final String BUILD_NUMBER = "20150929";
 }
