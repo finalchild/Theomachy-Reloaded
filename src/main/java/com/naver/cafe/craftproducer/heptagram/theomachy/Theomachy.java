999e12bf4098b270fba2540641b4a0712f8cb46b
Final Child <fcreloaded@outlook.kr>
2015-09-29 15:27:30 +0900

    Added default arenas. No arena functionability at all

--- src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/Theomachy.java
+++ src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/Theomachy.java
@@ -1,12 +1,21 @@
 package com.naver.cafe.craftproducer.heptagram.theomachy;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.comze_instancelabs.minigamesapi.Arena;
+import com.comze_instancelabs.minigamesapi.ArenaSetup;
+import com.comze_instancelabs.minigamesapi.MinigamesAPI;
+import com.comze_instancelabs.minigamesapi.PluginInstance;
+import com.comze_instancelabs.minigamesapi.config.ArenasConfig;
+import com.comze_instancelabs.minigamesapi.util.Util;
+import com.comze_instancelabs.minigamesapi.util.Validator;
 import com.naver.cafe.craftproducer.heptagram.theomachy.db.AbilityData;
 import com.naver.cafe.craftproducer.heptagram.theomachy.db.PluginData;
 import com.naver.cafe.craftproducer.heptagram.theomachy.manager.CommandManager;
@@ -24,6 +33,8 @@ public class Theomachy extends JavaPlugin {
 	
     public static Logger log;
     public CommandManager cm;
+    
+    MinigamesAPI api = null;
 	
     public void onEnable() {
         Theomachy.log = this.getLogger();
@@ -31,12 +42,15 @@ public class Theomachy extends JavaPlugin {
         getLogger().info("등록된 능력");
         getLogger().info("신: " + AbilityData.GOD_ABILITY_NUMBER + ", 인간: " + AbilityData.HUMAN_ABILITY_NUMBER);
         getLogger().info("총합: " + AbilityData.ABILITY_NUMBER);
+        
         getLogger().info("플러그인 스크립트 적용중");
+		api = MinigamesAPI.setupAPI(this, "Theomachy-Reloaded");
+		PluginInstance pinstance = MinigamesAPI.pinstances.get(this);
+        pinstance.addLoadedArenas(loadArenas(this, pinstance.getArenasConfig()));;
+        getServer().getPluginManager().registerEvents(new EventManager(), this);
         cm = new CommandManager(this);
         ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.BLAZE_ROD)).shape(new String[] { "|", "|", "|"}).setIngredient('|', Material.STICK);
-
         getServer().addRecipe(recipe);
-        getServer().getPluginManager().registerEvents(new EventManager(), this);
 		
         getLogger().info("게임의 설정 불러오는 중");
         getConfig().options().copyDefaults(true);
@@ -49,7 +63,6 @@ public class Theomachy extends JavaPlugin {
         animal = getConfig().getBoolean("Spawn Animal");
         monster = getConfig().getBoolean("Spawn Monster");
         difficulty = getConfig().getInt("Difficulty");
-		
         getLogger().info("---------------------------------------");
         getLogger().info("게임 시작 시 인벤토리 클리어 : " + String.valueOf(inventoryClear));
         getLogger().info("게임 시작 시 스카이블록 기본 아이템 지급 : " + String.valueOf(giveItem));
@@ -60,5 +73,26 @@ public class Theomachy extends JavaPlugin {
         getLogger().info("몬스터 스폰 : " + String.valueOf(monster));
         getLogger().info("난이도 : " + String.valueOf(difficulty));
         getLogger().info("---------------------------------------");
+    }
+    
+    public static ArrayList<Arena> loadArenas(JavaPlugin plugin, ArenasConfig cf) {
+        ArrayList<Arena> ret = new ArrayList<Arena>();
+        FileConfiguration config = cf.getConfig();
+        if (!config.isSet("arenas")) {
+            return ret;
+        }
+        for (String arena : config.getConfigurationSection("arenas.").getKeys(false)) {
+            if (Validator.isArenaValid(plugin, arena, cf.getConfig())) {
+                ret.add(initArena(plugin, arena));
+            }
+        }
+        return ret;
+    }
+    
+    public static TheomachyArena initArena(JavaPlugin plugin, String name) {
+        TheomachyArena arena = new TheomachyArena(plugin, name);
+		ArenaSetup setup = MinigamesAPI.pinstances.get(plugin).arenaSetup;
+        arena.init(Util.getSignLocationFromArena(plugin, name), Util.getAllSpawns(plugin, name), Util.getMainLobby(plugin), Util.getComponentForArena(plugin, name, "lobby"), setup.getPlayerCount(plugin, name, true), setup.getPlayerCount(plugin, name, false), setup.getArenaVIP(plugin, name));
+        return arena;
     }
 }
---
+++ src/main/java/com/naver/cafe/craftproducer/heptagram/theomachy/TheomachyArena.java
@@ -0,0 +1,13 @@
+package com.naver.cafe.craftproducer.heptagram.theomachy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.comze_instancelabs.minigamesapi.Arena;
+
+public class TheomachyArena extends Arena {
+
+	public TheomachyArena(JavaPlugin plugin, String name) {
+		super(plugin, name);
+	}
+	
+}
